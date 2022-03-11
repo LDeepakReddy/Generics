5c4a39e6288f98ec28d46b832990841d5c9e88de
LDeepakReddy <l.deepakreddy.com>
2022-03-11 06:38:43 +0530

    Array printing with generics

--- src/com/blz_generics/ArrayPrint.java
+++ src/com/blz_generics/ArrayPrint.java
@@ -1,51 +1,19 @@
 package com.blz_generics;
 
-public class ArrayPrint {
-    Integer intArray[];
-    Double doubleArray[];
-    Character charArray[];
+public class ArrayPrint<T> {
 
-    ArrayPrint(Integer[] intArray) {
-        this.intArray = intArray;
-    }
-
-    ArrayPrint(Double[] doubleArray) {
-        this.doubleArray = doubleArray;
-    }
-
-    ArrayPrint(Character[] charArray) {
-        this.charArray = charArray;
-    }
-
-    public void toPrint(Integer[] intArray) {
-        for (Integer value : intArray) {
-            System.out.println(value + " ");
-        }
-    }
-
-    public void toPrint(Double[] doubleArray) {
-        for (Double value : doubleArray) {
-            System.out.println(value + " ");
-        }
-    }
-
-    public void toPrint(Character[] charArray) {
-        for (Character value : charArray) {
+    public static <T> void toPrint(T[] array) {
+        for (T value : array) {
             System.out.println(value + " ");
         }
     }
 
     public static void main(String[] args) {
-        Integer intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        ArrayPrint printIntArray = new ArrayPrint(intArray);
-        printIntArray.toPrint(printIntArray.intArray);
+        Integer array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        ArrayPrint.toPrint(array);
+        Double doubleArray[] = {1.5, 2.0, 3.6, 4.8, 5.9};
+        ArrayPrint.toPrint(doubleArray);
 
-        Double doubleArray[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
-        ArrayPrint printDoubleArray = new ArrayPrint(doubleArray);
-        printDoubleArray.toPrint(printDoubleArray.doubleArray);
 
-        Character charArray[] = {'a', 'b', 'c', 'd'};
-        ArrayPrint printCharArray = new ArrayPrint((charArray));
-        printCharArray.toPrint(printCharArray.charArray);
     }
 }
