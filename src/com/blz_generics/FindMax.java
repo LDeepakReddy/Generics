2266a0337b04af4e1556394c6115988396590888
LDeepakReddy <l.deepakreddy.com>
2022-03-11 06:52:17 +0530

    print the Max Float

--- src/com/blz_generics/FindMax.java
+++ src/com/blz_generics/FindMax.java
@@ -2,7 +2,9 @@ package com.blz_generics;
 
 public class FindMax {
     public static void main(String[] args) {
-        Integer num1 = 10, num2 = 5, num3 = 8;
+        Integer num1 = 100, num2 = 5, num3 = 8;
+        Float float1 = 95.4f, float2 = 50.5f, float3 = 80.7f;
+        //String string1 = "car", string2 = "Aeroplane", string3 = "Bike";
         int maxInt = num1;
         if (num2.compareTo(maxInt) > 0) {
             maxInt = num2;
@@ -10,7 +12,16 @@ public class FindMax {
         if (num3.compareTo(maxInt) > 0) {
             maxInt = num3;
         }
-        System.out.println(maxInt + " is the maximum  ");
+        System.out.println(maxInt + " is the maximum number.");
+
+        float maxFloat = float1;
+        if (float2.compareTo(maxFloat) > 0) {
+            maxFloat = float2;
+        }
+        if (float3.compareTo(maxFloat) > 0) {
+            maxFloat = float3;
+        }
+        System.out.println(maxFloat + " is the maximum float.");
     }
 }
 
