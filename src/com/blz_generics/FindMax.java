f86bcf130c4ff48b1d6d6c6836a0d9f98d7f93c9
LDeepakReddy <l.deepakreddy.com>
2022-03-11 07:15:47 +0530

    Refracted code with generic method

--- src/com/blz_generics/FindMax.java
+++ src/com/blz_generics/FindMax.java
@@ -1,37 +1,35 @@
 package com.blz_generics;
 
 public class FindMax {
+
+    public static <T extends Comparable<T>> void  compare(T x,T y,T z){
+        T max =x;
+        if (y.compareTo(max)>0) {
+            max = y;
+        }
+        if (z.compareTo(max)>0){
+            max=z;
+        }
+        System.out.println(max + " is the maximum ");
+    }
+
     public static void main(String[] args) {
         Integer num1 = 100, num2 = 5, num3 = 8;
+        FindMax.compare(num1,num2,num3);
         Float float1 = 95.4f, float2 = 50.5f, float3 = 80.7f;
+        FindMax.compare(float1,float2,float3);
         String string1 = "car", string2 = "Aeroplane", string3 = "Bike";
-        int maxInt = num1;
-        if (num2.compareTo(maxInt) > 0) {
-            maxInt = num2;
-        }
-        if (num3.compareTo(maxInt) > 0) {
-            maxInt = num3;
-        }
-        System.out.println(maxInt + " is the maximum number.");
+        FindMax.compare(string1,string2,string3);
 
-        float maxFloat = float1;
-        if (float2.compareTo(maxFloat)>0){
-            maxFloat=float2;
-        }
-        if (float3.compareTo(maxFloat)>0){
-            maxFloat=float3;
-        }
-        System.out.println(maxFloat + " is the maximum float.");
-
-
-        if (string1.length() > string2.length() && string1.length() > string3.length()) {
-
-            System.out.println(string1 + " is the maximum");
-        } else if (string2.length() > string3.length()) {
-            System.out.println(string2 + " is the maximum");
-        } else {
-            System.out.println(string3 + " is the maximum");
-        }
+//
+//        if (string1.length() > string2.length() && string1.length() > string3.length()) {
+//
+//            System.out.println(string1 + " is the maximum");
+//        } else if (string2.length() > string3.length()) {
+//            System.out.println(string2 + " is the maximum");
+//        } else {
+//            System.out.println(string3 + " is the maximum");
+//        }
     }
 }
 
